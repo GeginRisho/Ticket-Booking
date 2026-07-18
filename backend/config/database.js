fd60d5e4c8c03e49382b48f401372f3e9ae50021
Gegin Risho <geginrisho@gmail.com>
2026-07-18 18:27:37 +0530

    Support Neon DATABASE_URL

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,15 +1,31 @@
-const { Sequelize } = require('sequelize');
-const dbConfig = require('./dbConfig');
-require('dotenv').config();
+const { Sequelize } = require("sequelize");
+const dbConfig = require("./dbConfig");
+require("dotenv").config();
 
-const env = process.env.NODE_ENV || 'development';
-const config = dbConfig[env];
+const env = process.env.NODE_ENV || "development";
 
-const sequelize = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
-  config
-);
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    protocol: "postgres",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
+} else {
+  const config = dbConfig[env];
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
 
 module.exports = sequelize;
